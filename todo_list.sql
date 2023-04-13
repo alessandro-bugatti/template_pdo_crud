b5b3540a4815518991c8634309f9e2ff48604044
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2023-04-13 11:40:34 +0200

    Aggiorna la struttura del database per gestire gli utenti

--- todo_list.sql
+++ todo_list.sql
@@ -28,9 +28,49 @@ CREATE TABLE `todo` (
   `data` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `completato` tinyint(1) NOT NULL DEFAULT 0,
   `importanza` int(11) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `id_user` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `todo_user_id_fk` (`id_user`),
+  CONSTRAINT `todo_user_id_fk` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todo`
+--
+
+LOCK TABLES `todo` WRITE;
+/*!40000 ALTER TABLE `todo` DISABLE KEYS */;
+INSERT INTO `todo` VALUES (2,'Aggiustare il lavandino del bagno della cantina','2023-04-13 09:36:34',0,4,1),(3,'Portare fuori lo sporco','2023-04-13 09:36:34',1,1,2),(4,'Completare l\'iscrizione di Chiara','2023-04-13 09:36:34',1,1,1),(8,'Pulire la macchina','2023-04-13 09:36:34',0,1,1),(11,'Comprare le pastiglie della dentiera','2023-04-13 09:36:34',1,3,2),(12,'Consegnare le verifiche in presidenza','2023-04-13 09:36:34',0,2,2),(13,'Compilare il 730','2023-04-13 09:36:34',0,3,1);
+/*!40000 ALTER TABLE `todo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `displayed_name` varchar(100) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_pk` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Piero','piero','a'),(2,'Anna','anna','a');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -41,4 +81,4 @@ CREATE TABLE `todo` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-28 11:28:23
+-- Dump completed on 2023-04-13 11:39:23
